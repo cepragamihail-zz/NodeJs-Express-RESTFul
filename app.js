c750e07af3fc32e33472902ad864212d43f6d501
Cepraga Mihail <yahim09@gmail.com>
2022-02-11 11:03:09 +0200

    install nodemon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
+  res.send('Welcome to my API!!!');
 });
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
