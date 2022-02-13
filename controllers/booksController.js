1565d3c21611c878598cb2e3d4684a1af90292e3
Cepraga Mihail <yahim09@gmail.com>
2022-02-13 20:44:48 +0200

    Adding Hypermedia

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -10,11 +10,21 @@ function booksController(Book) {
     return res.json(book);
   }
   function get(req, res) {
-    Book.find((err, books) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(books);
+      const returnBooks = books.map((book) => {
+        const newBook = book.toJSON();
+        newBook.links = {};
+        newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
+        return newBook;
+      })
+      return res.json(returnBooks);
     });
   }
   return { post, get };
